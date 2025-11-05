3332893037283f64ff6312a0c15ed00fc4651de2
Richard Hull <rm_hull@yahoo.co.uk>
2025-11-05 00:21:02 +0000

    test: Fix assertions for multiple DNS questions
    Removes temporary assertions checking for incorrect NXDOMAIN
    behavior in `TestDNSDispatcher_HandleDNSRequest_MultipleQuestions`.
    
    The test now correctly asserts that a successful response
    (`dns.RcodeSuccess`) is returned upon dispatching, along with
    the expected number of answers.

--- internal/dns_test.go
+++ internal/dns_test.go
@@ -108,20 +108,10 @@ func TestDNSDispatcher_HandleDNSRequest_MultipleQuestions(t *testing.T) {
 	// Call the method under test
 	dispatcher.HandleDNSRequest(writer, req)
 
-	// FIXME: this is the correct/expected behaviour .. to be fixed in #3
-	// ==================================================================
 	// Assert that the response writer was called with a non-nil message
-	// assert.NotNil(t, writer.WrittenMsg)
-	// assert.Equal(t, dns.RcodeSuccess, writer.WrittenMsg.Rcode)
-	// assert.Len(t, writer.WrittenMsg.Answer, 1)
-	// assert.Len(t, writer.WrittenMsg.Question, 2)
-
-	// FIXME: current behaviour (to be removed)
-	// ================================================
-	// Assert that the response has an NXDOMAIN Rcode because one of the questions is blocked
 	assert.NotNil(t, writer.WrittenMsg)
-	assert.Equal(t, dns.RcodeNameError, writer.WrittenMsg.Rcode)
-	assert.Len(t, writer.WrittenMsg.Answer, 0)
+	assert.Equal(t, dns.RcodeSuccess, writer.WrittenMsg.Rcode)
+	assert.Len(t, writer.WrittenMsg.Answer, 6)
 	assert.Len(t, writer.WrittenMsg.Question, 1)
 }
 
