1efbe0f3d0f72a686e732c8aaf6f83d731d5fe5f
Richard Hull <rm_hull@yahoo.co.uk>
2026-06-12 17:36:37 +0100

    chore: harden TLS configuration for server
    - Explicitly set `MaxVersion` to TLS 1.3.
    - Enforce secure cipher suites for ECDHE and AEAD.
    - Enable `PreferServerCipherSuites` to prioritize server-side security.

--- internal/app.go
+++ internal/app.go
@@ -306,7 +306,17 @@ func (app *App) RunServer(ctx context.Context) error {
 				return err
 			}
 			listener = tls.NewListener(proxyListener, &tls.Config{
-				MinVersion:     tls.VersionTLS12,
+				MinVersion:               tls.VersionTLS12,
+				MaxVersion:               tls.VersionTLS13,
+				PreferServerCipherSuites: true,
+				CipherSuites: []uint16{
+					tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+					tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+					tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+					tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+				},
 				NextProtos:     []string{"dot"},
 				GetCertificate: magic.GetCertificate,
 			})
