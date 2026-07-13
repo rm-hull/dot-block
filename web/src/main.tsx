e6e57968dfcce9773aa9b2bd2ab94eca3f4ce8a7
Richard Hull <rm_hull@yahoo.co.uk>
2026-07-13 22:11:26 +0100

    feat: add @tanstack/react-query-devtools
    Integrate React Query Devtools into the application root to enable
    debugging and monitoring of server state queries during development.

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider, createRouter } from "@tanstack/react-router"
 import React from "react"
 import ReactDOM from "react-dom/client"
@@ -17,6 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
 )
