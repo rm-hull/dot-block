51547d85f2fb4694b9a7045cd9a6c331d9976c9e
Richard Hull <rm_hull@yahoo.co.uk>
2026-07-15 22:53:19 +0100

    rfix: silence linter on port listener close
    Address potential linter warnings by explicitly ignoring the error
    return
    value when closing the listener in `getFreePort`.

--- internal/integration_test.go
+++ internal/integration_test.go
@@ -21,7 +21,7 @@ func getFreePort(t *testing.T) int {
 	t.Helper()
 	l, err := net.Listen("tcp", "127.0.0.1:0")
 	require.NoError(t, err)
-	defer l.Close()
+	defer func() { _ = l.Close() }()
 	return l.Addr().(*net.TCPAddr).Port
 }
 
